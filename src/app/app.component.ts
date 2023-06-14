e8d7e721868ff9d6b29e7d0403398ba3bb25b32d
Hazzard1912 <jhonnier.hernandez@correounivalle.edu.co>
2023-06-13 19:39:51 -0500

    Se hizo el navbar, la pagina principal y el footer, falta aun implementar las funcionalidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'GameQuest';
-}
+export class AppComponent {}
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MenubarModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [
+        {
+            label: 'Mejores Valorados',
+            icon: 'pi pi-fw pi-star',
+        },
+        {
+            label: 'Generos',
+            icon: 'pi pi-fw pi-hashtag',
+            items: [
+                {
+                    label: 'Accion',
+                },
+                {
+                    label: 'Indie',
+                },
+                {
+                    label: 'Aventura',
+                },
+                {
+                    label: 'Rol',
+                },
+                {
+                    label: 'Shooter',
+                },
+                {
+                    label: 'Casual',
+                },
+                {
+                    label: 'Simulacion',
+                },
+                {
+                    label: 'Puzzle',
+                },
+                {
+                    label: 'Arcade',
+                },
+                {
+                    label: 'Plataformas',
+                },
+                {
+                    label: 'MMO',
+                },
+                {
+                    label: 'Carreras',
+                },
+                {
+                    label: 'Deportes',
+                },
+                {
+                    label: 'Peleas',
+                },
+                {
+                    label: 'De Cartas',
+                },
+                {
+                    label: 'Estrategia',
+                }
+            ]
+        },
+        {
+            label: 'Registrarse',
+            icon: 'pi pi-fw pi-user-plus',
+        },
+        {
+            label: 'Loguearse',
+            icon: 'pi pi-fw pi-sign-in',
+        },
+        {
+            label: 'Salir',
+            icon: 'pi pi-fw pi-power-off'
+        }
+    ];
+}
+
+// TODO: Implementar el servicio para hacer la busqueda en la api
+// y asi poder realizar las busquedas desde el navbar...
+
+// Todos los botones ejecutaran el mismo metodo, metodo que pedira
+// por un string que asociaremos al boton, y podremos entonces usar
+// un switch case para decidir que peticion hacer. 
 
 }
